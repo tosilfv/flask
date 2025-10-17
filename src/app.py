6b338c88ec84ebe05bd8428e8aa65aa0926db9df
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-10-17 05:25:56 +0300

    Initial upload

---
+++ src/app.py
@@ -0,0 +1,13 @@
+# Go to address http://127.0.0.1:5000/ to see the Flask app in action.
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
