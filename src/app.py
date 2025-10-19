848608bfcf60cd609fe59b5b4deeaccb6fe83a2c
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-10-19 07:57:33 +0300

    u3

--- src/app.py
+++ src/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/test')
+def test():
+    return 'test'
+
 if __name__ == '__main__':
     app.run(debug=True)
  
