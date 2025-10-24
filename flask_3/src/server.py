71355001da28841bf83e0ba9b707358430641b17
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-10-24 04:30:06 +0300

    u8

--- flask_3/src/server.py
+++ flask_3/src/server.py
@@ -1,10 +1,14 @@
+import random, datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', rand_num = random_number, \
+                           cur_y=current_year)
 
 if __name__ == '__main__':
     app.run(debug=True)
