9adc35cd2fcd8600b8a70ce1fcc1d3f54878604e
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-10-25 04:27:13 +0300

    u9

--- flask_3/src/server.py
+++ flask_3/src/server.py
@@ -1,7 +1,16 @@
 import random, datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 123
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your_app_password'
+mail = Mail(app)
 
 @app.route('/')
 def home():
@@ -9,6 +18,21 @@ def home():
     current_year = datetime.datetime.now().year
     return render_template('index.html', rand_num = random_number, \
                            cur_y=current_year)
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    
+    msg = Message('New Message from Contact Form',
+                  sender='your_email@gmail.com',
+                  recipients=['recipient@example.com'])
+    msg.body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+    mail.send(msg)
+    
+    flash('Email sent successfully!')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
