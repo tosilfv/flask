8cf68090ea9d49536eb8664c24e478786c4539b0
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-10-27 05:26:11 +0200

    u10

--- flask_3/src/server.py
+++ flask_3/src/server.py
@@ -1,8 +1,10 @@
+import requests
 import random, datetime
 from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
+QUOTE_URL = 'https://zenquotes.io/api/random'
 app.secret_key = 'your_secret_key'
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -18,6 +20,15 @@ def home():
     current_year = datetime.datetime.now().year
     return render_template('index.html', rand_num = random_number, \
                            cur_y=current_year)
+
+@app.route('/quotes')
+def home():
+    current_year = datetime.now().year
+    res = requests.get(QUOTE_URL)
+    res_json = res.json()
+    return render_template('index.html', cur_y=current_year, \
+                            quote_q=res_json[0]['q'], \
+                            quote_a=res_json[0]['a'])
 
 @app.route('/send-email', methods=['POST'])
 def send_email():
